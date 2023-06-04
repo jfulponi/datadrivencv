e36c90e22976f0e2b7593e9fb457c4f0458576d8
Juan Ignacio Fulponi <61839284+jfulponi@users.noreply.github.com>
2023-06-04 17:06:06 -0300

    Update cv_network.js

--- inst/js/cv_network.js
+++ inst/js/cv_network.js
@@ -1,104 +1,32 @@
-const data_json = document.querySelector("script[type='application/json']").textContent;
-const {edges, nodes} = JSON.parse(data_json);
 
-class MyHandler extends Paged.Handler {
-	constructor(chunker, polisher, caller) {
-		super(chunker, polisher, caller);
-	}
+var projection = d3.geoOrthographic()
+    .scale(248)
+    .rotate([0, 0])
+    .translate([width / 2, height / 2])
+    .clipAngle(90);
 
-	afterRendered (){
-		plot_network();
-	}
-}
-Paged.registerHandlers(MyHandler);
-
-function plot_network(){
   const {width, height} = document.querySelector("svg#cv_network_viz").getBoundingClientRect();
 
   const svg = d3.select("svg#cv_network_viz")
     .attr("width", width)
     .attr("height", height);
 
-  const unique_sections = [...new Set(nodes.map(d => d.section))];
-  const color_scale = d3.scaleOrdinal()
-    .domain(unique_sections)
-    .range(d3.schemeSet2);
+var c = canvas.node().getContext("2d");
 
-  const edge_color = d3.scaleLinear()
-    .domain(d3.extent(edges, d => d.year));
+var path = d3.geoPath()
+    .projection(projection)
+    .context(c);
 
-  const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(edges).id(d => d.id))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .on("tick", ticked);
+d3.json("https://d3js.org/world-110m.v1.json", function(error, world) {
+  if (error) throw error;
 
-  const g = svg.append("g");
+  var land = topojson.feature(world, world.objects.land);
 
-  const link = g
-    .selectAll("line")
-    .data(edges)
-    .enter().append("line")
-      .attr("stroke", d => d3.interpolateGreys(edge_color(d.year)))
-      .attr("stroke-width", 0.5);
+  d3.timer(function(elapsed) {
+    c.clearRect(0, 0, width, height);
 
-  const node = g
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", d => color_scale(d.section))
-      .call(drag(simulation));
+    projection.rotate([elapsed / 150, 0]);
 
-  node.append("title")
-      .text(d => `${d.section}\n${d.title}`);
-
-  svg.call(d3.zoom()
-      .extent([[0, 0], [width, height]])
-      .scaleExtent([1, 8])
-      .on("zoom", zoomed));
-
-  function ticked() {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-  }
-
-  function zoomed() {
-    g.attr("transform", d3.event.transform);
-  }
-
-  function drag(simulation){
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-  }
-
-}
+    c.fillStyle = "#fff", c.beginPath(), path(land), c.fill();
+  });
+});
